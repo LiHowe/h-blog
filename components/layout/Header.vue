e7f3264733303af30ec943af469ac38e830b31d9
Howe <howe.personal@gmail.com>
2021-01-06 09:47:53 +0800

    first init

---
+++ components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header-container">this is header</div>
+</template>
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+<style lang="scss" scoped>
+.header {
+  &-container {
+    display: block;
+  }
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="error-container">
+    <h1 v-if="error.statusCode === 404">Not found</h1>
+    <h1 v-else>{{ error }}</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  layout: 'default',
+}
+</script>
